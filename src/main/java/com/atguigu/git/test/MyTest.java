78f38d2b3b5f838fa34e2aaa68aa8a6a2d3e6fec
hxy <792729932@qq.com>
2023-01-01 16:58:33 +0800

    my first commit

---
+++ src/main/java/com/atguigu/git/test/MyTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git.test;
+
+/**
+ * @author hxy
+ * @creat 2023-01-01-16:39
+ */
+
+public class MyTest {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
